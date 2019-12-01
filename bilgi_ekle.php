18dcd535beb59cd0ce613e680bef0b67cc4f0e5f
Mansur Beştaş <mansur@siirt.edu.tr>
2019-12-01 22:27:14 +0300

    Update bilgi_ekle.php

--- bilgi_ekle.php
+++ bilgi_ekle.php
@@ -1,32 +1,27 @@
 <?php
-//error_reporting(0);
 require __DIR__ . '/vendor/autoload.php';
-use App\add_data;
-use App\client_data;
-use App\general;
 include "db_data.php";
 $min_minute_check = 1;
 
 if(!empty($_GET['site_id'])) {
     $site_id = $_GET['site_id'];
     $site_id = 1;
-    $ip = (new client_data())->client_ip();
-    $referer = (new client_data())->page_adress();
-    $fullzaman = (new client_data())->fulldatetime();
+    $ip = (new App\client_data())->client_ip();
+    $referer = (new App\client_data())->page_adress();
+    $fullzaman = (new App\client_data())->fulldatetime();
 
-    $varmi = (new add_data())->varmi($db_info, $ip, $site_id, $referer);
+    $varmi = (new App\add_data())->varmi($db_info, $ip, $site_id, $referer);
 
-    if ($varmi == 0) {
-        $cevap = (new add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
+    if ($varmi == 0)
+    {
+        $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
     } else {
-        $son_gorulme_zamani = (new add_data())->son_gorulme_zamani($db_info, $ip, $site_id, $referer);
-        $zaman_farki = (new general())->dateDifference($son_gorulme_zamani, $fullzaman);
+        $son_gorulme_zamani = (new App\add_data())->son_gorulme_zamani($db_info, $ip, $site_id, $referer);
+        $zaman_farki = (new App\general())->dateDifference($son_gorulme_zamani, $fullzaman);
         if ((int)$zaman_farki > (int)$min_minute_check) {
-            $cevap = (new add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
+            $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
         } else {
-            $etkilenen = (new add_data())->update_record($db_info, $ip, $fullzaman, $site_id,$varmi);
+            $etkilenen = (new App\add_data())->update_record($db_info, $ip, $fullzaman, $site_id,$varmi);
         }
     }
 }
-
-?>
