921aeb4698b4198e9b73f9b9e769256a9f9ed8b9
Mansur Beştaş <mansur@siirt.edu.tr>
2019-12-01 22:32:17 +0300

    Update bilgi_ekle.php

--- bilgi_ekle.php
+++ bilgi_ekle.php
@@ -8,20 +8,21 @@ if(!empty($_GET['site_id'])) {
     $site_id = 1;
     $ip = (new App\client_data())->client_ip();
     $referer = (new App\client_data())->page_adress();
-    $fullzaman = (new App\client_data())->fulldatetime();
+    $fulltime = (new App\client_data())->fulldatetime();
 
     $varmi = (new App\add_data())->varmi($db_info, $ip, $site_id, $referer);
 
     if ($varmi == 0)
     {
-        $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
+        $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fulltime, $site_id);
     } else {
         $son_gorulme_zamani = (new App\add_data())->son_gorulme_zamani($db_info, $ip, $site_id, $referer);
-        $zaman_farki = (new App\general())->dateDifference($son_gorulme_zamani, $fullzaman);
+        $zaman_farki = (new App\general())->dateDifference($son_gorulme_zamani, $fulltime);
         if ((int)$zaman_farki > (int)$min_minute_check) {
-            $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fullzaman, $site_id);
+            $cevap = (new App\add_data())->insert_record($db_info, $ip, $referer, $fulltime, $site_id);
         } else {
-            $etkilenen = (new App\add_data())->update_record($db_info, $ip, $fullzaman, $site_id,$varmi);
+            $etkilenen = (new App\add_data())->update_record($db_info, $ip, $fulltime, $site_id,$varmi);
         }
     }
 }
+
